fa8f393c488bcd5a281601b8132fd4916b201214
Sanjit Hyanju <sanjithyanju99@gmail.com>
2024-08-29 18:48:31 +0545

    Guessing the random number by User

---
+++ Guess_the_Number_User.py
@@ -0,0 +1,20 @@
+import random
+
+def guess(x):
+    low = 1
+    high = x
+    feedback = ""
+    while feedback != "c":
+        if low!=high:
+            guess = random.randint(low, high)
+        else:
+            guess = low 
+        feedback = input(f"Is {guess} too high (H) or too low (L) or corrent (C)? ").lower()
+        if feedback == "h":
+            high = guess - 1 
+        elif feedback == "l":
+            low = guess + 1
+        
+    print(f"Computer has guessed the number {guess} correctly!")
+
+guess(100000000)
